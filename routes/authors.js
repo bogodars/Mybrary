a0e0d5f5831c415d82d6ade5329954b4e49cd39f
bogodars <bogodar@gmail.com>
2020-02-08 22:29:02 +0200

    Finished Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 // All Authors Route
 router.get('/',  async (req, res) => {
@@ -26,42 +27,78 @@ router.get('/new' , (req, res) => {
 })
 
 //Create Authors Route
-router.post('/', async (req,res) => {
-	const author = new Author({
-		name: req.body.name
-	})
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect('/authors')
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    })
+  }
+})
+// Show Author
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit Author
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+})
+
+// Update Author 
+router.put('/:id', async (req, res) => {
+	let author 
    try {
-		 const newAuthor = await author.save()
-		 		//  res.redirect(`authors/${newAuthor.id`)
-				 res.redirect(`authors`)	
-	 } catch {
-		  res.render('authors/new', {
+		author = await Author.findById(req.params.id)
+		author.name = req.body.name
+		await author.save()
+		res.redirect(`/authors/${author.id}`)
+	} catch {
+		if (author == null) {
+			res.redirect('/')
+		} else {
+		  res.render('authors/edit', {
 			author: author,
-			errorMessage: "Put valid name"
+			errorMessage: "Error updating Author"
 			})
+		}
+	 }
+})
+
+// Delete Author
+router.delete('/:id', async (req, res) => {
+	let author
+   try {
+		author = await Author.findById(req.params.id)
+		await author.remove()
+		res.redirect('/authors')
+	} catch {
+		if (author == null) {
+			res.redirect('/')
+		} else {
+		  res.redirect(`/authors/${author.id}`)
+		}
 	 }
 })
 
 module.exports = router
-
-
-
-
-// callback hell example 
-
-// router.post('/', (req,res) => {
-// 	const author = new Author({
-// 		name: req.body.name
-// 	})
-// 	author.save((err, newAuthor) => {
-// 		if (err) {
-// 		res.render('authors/new', {
-// 			author: author,
-// 			errorMessage: "Put valid name"
-// 			})
-// 		} else {
-// 				//  res.redirect(`authors/${newAuthor.id`)
-// 				 res.redirect(`authors`)	
-// 		}
-// 	})
-// })
